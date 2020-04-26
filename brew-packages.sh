752b7f32e7345ce18021e64fcda5c06a59707a9b
Carsten Zimmermann <cz@aegisnet.de>
2020-04-26 21:15:54 +0200

    Update brew packages

--- brew-packages.sh
+++ brew-packages.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-brew install git vim bash-completion pwgen imagemagick mutt postgres elinks gnupg ffmpeg coreutils \
-  pbzip2 ctags bash htop elixir heroku jq colordiff
+brew install git vim bash-completion pwgen imagemagick mutt elinks gnupg ffmpeg coreutils \
+  pbzip2 ctags bash htop jq colordiff coreutils postgresql@10 redis todo-txt
 
 
 echo
 echo Postinstall:
 echo
 echo brew services start postgresql
-echo 
+echo
